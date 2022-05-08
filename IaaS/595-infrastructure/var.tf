a8fde61fc50cef8afd17a6ab9c8e3d67baa74a92
Usman Aslam <Usman Aslam>
2022-05-08 14:23:19 -0700

    asd

--- IaaS/595-infrastructure/var.tf
+++ IaaS/595-infrastructure/var.tf
@@ -24,3 +24,13 @@ variable "public_vpc_priv_subnet_rt_name" {}
 
 variable "author" {}
 variable "team" {}
+#Golbal
+
+#VPC
+variable "private_vpc_cidr" {}
+variable "private_vpc_name" {}
+
+#Private Subnet
+variable "private_vpc_priv_subnet_cidr" {}
+variable "private_vpc_priv_subnet_name" {}
+variable "private_vpc_priv_subnet_rt_name" {}
