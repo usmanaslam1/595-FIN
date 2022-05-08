8388b9e9937170d4e996458148ef250ced6df91f
Usman Aslam <Usman Aslam>
2022-05-08 14:06:43 -0700

    ad

--- IaaS/internal-vpc/var.tf
+++ IaaS/internal-vpc/var.tf
@@ -4,17 +4,6 @@ variable "region" {}
 #VPC
 variable "private_vpc_cidr" {}
 variable "private_vpc_name" {}
-variable "private_vpc_igw_name" {}
-
-
-#Public Subnet
-variable "private_vpc_pub_subnet_cidr" {}
-variable "private_vpc_pub_subnet_name" {}
-variable "private_vpc_pub_subnet_rt_name" {}
-
-variable "private_vpc_nat_gw_name" {}
-variable "private_vpc_natgw_eip_name" {}
-
 
 #Private Subnet
 variable "private_vpc_priv_subnet_cidr" {}
